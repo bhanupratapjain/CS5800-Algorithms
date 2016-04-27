9fe08581927f929d29292221c9a9b166e180cc3a
Bhanu Jain <bhanujain88@gmail.com>
2016-04-26 20:38:00 -0400

    HW13-PB06

---
+++ PA/src/homework15/question06/EuclidGcd.java
@@ -0,0 +1,19 @@
+package homework15.question06;
+
+/**
+ * Created by Bhanu on 26/04/2016.
+ */
+public class EuclidGcd {
+
+    public static int gcd(int p, int q) {
+        if (q == 0) {
+            return p;
+        }
+        return gcd(q, p % q);
+    }
+
+    public static void main(String[] args){
+        System.out.println(gcd(4, 16) );
+        System.out.println(gcd(5, 25) );
+    }
+}
